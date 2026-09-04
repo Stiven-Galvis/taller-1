8339b48811b5216a3f380393f09d9db456e6041c
Stiven-Galvis <jhoan2252891@correo.uis.edu.co>
2026-09-04 12:01:08 -0500

    comence a hacer el mergesort

--- src/MisAlgoritmos.java
+++ src/MisAlgoritmos.java
@@ -14,24 +14,25 @@ public class MisAlgoritmos implements Busquedas, Ordenamientos {
     @Override
     public int binarySearch(int[] arrayPorExplorar, int elementoABuscar) {
         int inicio = 0;
-        int fin = arrayPorExplorar.length - 1;
+        int fin = arrayPorExplorar.length - 1 ;
 
-        while (inicio <= fin) {
-            int medio = (inicio + fin / 2);
+        while(inicio <=fin)
 
-            if (arrayPorExplorar[medio] ==
-                    elementoABuscar) {
-                return medio;
+    {
+        int medio = (inicio + fin / 2) ;
+
+        if (arrayPorExplorar[medio] ==
+                elementoABuscar) {
+            return medio;
             }
         }
-
-
+        return inicio;
     }
 
     @Override
     public int[] bubbleSort(int[] arrayDesordenado) {
         for (int i = 0; i < arrayDesordenado.length - 1; i++) {
-            for (int i = 0; i < arrayDesordenado.length - 1 - i; i++) {
+            for (i = 0; i < arrayDesordenado.length - 1 - i; i++) {
                 if (arrayDesordenado[i] > arrayDesordenado[i + 1]) {
                     int temporal = arrayDesordenado[i];
                     arrayDesordenado[i] = arrayDesordenado[i + 1];
@@ -67,13 +68,37 @@ public class MisAlgoritmos implements Busquedas, Ordenamientos {
 
     @Override
     public int[] insertionSort(int[] arrayDesordenado) {
+        if (arrayDesordenado == null) {
+            return null ;
+        }
 
-        return new int[0];
+        int n = arrayDesordenado.length ;
+
+        for (int i = 1 ; i < n ; i++) {
+            int clave = arrayDesordenado[i] ;
+            int j = i - 1 ;
+
+            while (j >= 0 && arrayDesordenado[j] > clave) {
+                arrayDesordenado[j + 1] = arrayDesordenado[j] ;
+                j= j - 1 ;
+            }
+            arrayDesordenado[j + 1] = clave ;
+        }
+        return arrayDesordenado ;
     }
 
     @Override
     public int[] mergeSort(int[] arrayDesordenado) {
-        return new int[0];
+        if (arrayDesordenado == null || arrayDesordenado.length <= 1) {
+            return  arrayDesordenado ;
+        }
+        int mitad = arrayDesordenado.length / 2 ;
+        int[] izquierda = new int[mitad] ;
+        int[] derecha = new int [arrayDesordenado.length - mitad] ;
+
+        System.arraycopy(arrayDesordenado, 0, izquierda, 0, mitad) ;
+        System.arraycopy(arrayDesordenado, mitad, derecha, 0, arrayDesordenado,
+        return new int[0] ;
     }
 
     @Override
